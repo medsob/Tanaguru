8c7e33b83a2fff18586e9f015cfb56e5a23b5d80
SOBAHI MOHAMED <medsob@gmail.com>
2015-10-14 11:54:03 +0200

    maj des dépendences pour lancer tanaguru-kafka

--- engine/tanaguru-kafka/src/main/java/org/tanaguru/kafka/consumer/KafkaConsumer.java
+++ engine/tanaguru-kafka/src/main/java/org/tanaguru/kafka/consumer/KafkaConsumer.java
@@ -10,56 +10,86 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-import org.apache.log4j.spi.LoggerFactory;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.integration.channel.QueueChannel;
 import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
 
 /**
  *
  * @author tanaguru
  */
+@Component
 public class KafkaConsumer {
-    private static final String CONFIG = "/consumer_context.xml";
-	private static Random rand = new Random();
-	@SuppressWarnings({ "unchecked", "unchecked", "rawtypes" })
-	public static void main(String[] args) {
-		
-		final ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(CONFIG, KafkaConsumer.class);
-		ctx.start();
-		final QueueChannel channel = ctx.getBean("inputFromKafka", QueueChannel.class);
-		Message msg;		
-		while((msg = channel.receive()) != null) {
-			HashMap map = (HashMap)msg.getPayload();
-			Set<Map.Entry> set = map.entrySet();
-			for (Map.Entry entry : set) {
-				String topic = (String)entry.getKey();
-				System.out.println("Topic:" + topic);
-				ConcurrentHashMap<Integer,List<byte[]>> messages = (ConcurrentHashMap<Integer,List<byte[]>>)entry.getValue();
-				Collection<List<byte[]>> values = messages.values();
-				for (Iterator<List<byte[]>> iterator = values.iterator(); iterator.hasNext();) {
-					List<byte[]> list = iterator.next();
-					for (byte[] object : list) {
-						String message = new String(object);
-						System.out.println("\tMessage: " + message);
-					}
-					
-				}
-			
-			}
-			
-		}
-		
-		try {
-			Thread.sleep(100000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		ctx.close();
-	}
+
+    private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+//    @Autowired
+//    @Qualifier("inputFromKafka")
+//    QueueChannel channel;
+//
+//    public void consumeMessage() {
+//        Message msg;
+//        while ((msg = channel.receive()) != null) {
+//            HashMap map = (HashMap) msg.getPayload();
+//            Set<Map.Entry> set = map.entrySet();
+//            for (Map.Entry entry : set) {
+//                String topic = (String) entry.getKey();
+//                System.out.println("Topic:" + topic);
+//                ConcurrentHashMap<Integer, List<byte[]>> messages = (ConcurrentHashMap<Integer, List<byte[]>>) entry.getValue();
+//                Collection<List<byte[]>> values = messages.values();
+//                for (Iterator<List<byte[]>> iterator = values.iterator(); iterator.hasNext();) {
+//                    List<byte[]> list = iterator.next();
+//                    for (byte[] object : list) {
+//                        String message = new String(object);
+//                        System.out.println("\tMessage: " + message);
+//                    }
+//
+//                }
+//
+//            }
+//        }
+//
+//        try {
+//            Thread.sleep(100000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+    public void processMessage(Map<String, Map<Integer, List<byte[]>>> msgs) {
+        for (Map.Entry<String, Map<Integer, List<byte[]>>> entry : msgs
+                .entrySet()) {
+            log.debug("Topic:" + entry.getKey());
+            ConcurrentHashMap<Integer, List<byte[]>> messages = (ConcurrentHashMap<Integer, List<byte[]>>) entry
+                    .getValue();
+            log.debug("\n**** Partition: \n");
+            Set<Integer> keys = messages.keySet();
+            for (Integer i : keys) {
+                log.debug("p:" + i);
+            }
+            log.debug("\n**************\n");
+            Collection<List<byte[]>> values = messages.values();
+            for (Iterator<List<byte[]>> iterator = values.iterator(); iterator
+                    .hasNext();) {
+                List<byte[]> list = iterator.next();
+                for (byte[] object : list) {
+                    String message = new String(object);
+                    log.debug("Message: " + message);
+                    try {
+                        System.out.println(message);
+                    } catch (Exception e) {
+                        log.error(String.format("Failed to process message %s",
+                                message));
+                    }
+                }
+            }
+
+        }
+    }
+
 }
--- engine/tanaguru-kafka/src/main/java/org/tanaguru/kafka/producer/KafkaProducer.java
+++ engine/tanaguru-kafka/src/main/java/org/tanaguru/kafka/producer/KafkaProducer.java
@@ -5,30 +5,46 @@
  */
 package org.tanaguru.kafka.producer;
 
+import com.yammer.metrics.Metrics;
 import java.util.Random;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import javax.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.integration.kafka.support.KafkaHeaders;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.stereotype.Component;
+import org.tanaguru.kafka.consumer.KafkaConsumer;
 
 /**
  *
  * @author tanaguru
  */
+@Component
 public class KafkaProducer {
-    private static final String CONFIG = "/context.xml";
-	private static Random rand = new Random();
-	public static void main(String[] args) {
-		final ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(CONFIG, KafkaProducer.class);
-		ctx.start();
-		final MessageChannel channel = ctx.getBean("inputToKafka", MessageChannel.class);
-		for (int i = 0; i < 100; i++) {
-			channel.send(MessageBuilder.withPayload("Message-" + rand.nextInt()).setHeader("messageKey", String.valueOf(i)).setHeader("topic", "test").build());
-		}
-		try {
-			Thread.sleep(100000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		ctx.close();
-	}
+
+    private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+    private static Random rand = new Random();
+
+    @Autowired
+    private MessageChannel inputToKafka;
+
+    @PostConstruct
+    public void initMethod() throws Exception {
+        log.info("Bean constructed");
+        Metrics.shutdown();
+    }
+
+    public void produceMessage(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            inputToKafka.send(MessageBuilder.withPayload("Message-" + rand.nextInt()).setHeader(KafkaHeaders.MESSAGE_KEY, String.valueOf(i)).setHeader(KafkaHeaders.TOPIC, "messageExemple").build());
+        }
+        try {
+            Thread.sleep(100000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
